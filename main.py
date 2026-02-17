320c38448a0413730f07f0a9502126f853052873
AJ23-coder <aayush338jha@eng.rizvi.edu.in>
2026-02-17 22:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Aayush Jha
+# Date: 10/02/2026
 
 print("--- Extracting Words from Text File ---\n")
+
+with open("story.txt","r") as file:
+  text=file.read().split()
+  length=int(input("Enter Length of Words: ")
+  list1=[]
+  for x in text:
+    if (len(x)==length):
+      list1.append(x).lower()
+  new_list=sorted(set(list))
+  print(f"Words with length {length} are: {new_list}")
 
