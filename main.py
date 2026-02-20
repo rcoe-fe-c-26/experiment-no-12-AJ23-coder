90013783a3dcc7d66bf49b58ce140ed1832049e1
AJ23-coder <aayush338jha@eng.rizvi.edu.in>
2026-02-20 22:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ print("--- Extracting Words from Text File ---\n")
 
 with open("story.txt","r") as file:
   text=file.read().split()
-  length=int(input("Enter Length of Words: ")
+  length=int(input("Enter Length of Words: "))
   list1=[]
   for x in text:
     if (len(x)==length):
       list1.append(x).lower()
   new_list=sorted(set(list1))
   print(f"Words with length {length} are: {new_list}")
+
 
 
